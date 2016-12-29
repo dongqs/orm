d03be6ba6831b3a2737e6a761dc2af66dfb27aeb
Qingshan <dongqs@sunftech.cn>
2016-12-29 15:39:25 +0800

    what is wrong

--- src/main.cpp
+++ src/main.cpp
@@ -16,12 +16,33 @@ private:
     const string name_;
 };
 
+// with inheritance
+class Base
+{
+public:
+    template <typename F>
+    auto run(F f) { return bind(f, this)(); }
+};
+
+class Derived : public Base
+{
+public:
+    Derived(const string& name) : name_(name) {}
+    const string& name() const { return name_; }
+private:
+    const string name_;
+};
+
 int main()
 {
-    const auto basic = make_shared<Basic>("pp");
+    const auto basic = make_shared<Basic>("without inheritance");
 
     cout << basic->name() << endl;
     cout << basic->run(&Basic::name) << endl;
 
+    const auto derived = make_shared<Derived>("inheritance");
+
+    cout << derived->name() << endl;
+    cout << derived->run(&Derived::name) << endl;
     return 0;
 }
